2b80f929cdf186c6970d3b9beb23e335b5b2e7f5
Haggag <https://github.com/yasmeenhaggag>
2023-04-16 11:18:09 +0200

    week1

--- Week3Tasks/Week3/src/com/week3/BankApplication.java
+++
@@ -1,8 +0,0 @@
-package com.week3;
-import java.util.Scanner;
-public class BankApplication {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-    }
-}
--- Week3Tasks/Week3/src/com/week3/SavingsAccount.java
+++
@@ -1,9 +0,0 @@
-package com.week3;
-
-public class SavingsAccount extends Account {
-    double interestRate,interestBalance;
-    public void calInterestBalance(){
-        interestBalance=balance*interestRate;
-    }
-
-}
