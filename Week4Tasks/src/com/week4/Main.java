dd453b67b5be4334db59386a82c4733bce2b6425
Yasmin Haggag <yasmeenhaggag3@gmail.com>
2023-05-29 01:34:43 +0300

    resolved

---
+++ Week4Tasks/src/com/week4/Main.java
@@ -0,0 +1,29 @@
+package com.week4;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        StringOccurence s=new StringOccurence();
+        System.out.println(s.occurence("Yasmin Yasmin Haggag","Yasmin"));
+
+        /*int num1=0,num2=0; boolean flag=true;
+        while(flag){
+        try{
+             num1=input.nextInt();
+             num2=input.nextInt();
+             flag=false;
+        }
+        catch (Exception e){
+            System.out.println("Enter numeric value");
+            input.nextLine();
+        }
+        }
+        System.out.println(num1+num2);*/
+
+
+
+
+    }}
