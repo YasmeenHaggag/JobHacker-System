3fcf806dfd4fc0b9e3d5bf605a8b765010259b7f
Yasmin Haggag <yasmeenhaggag3@gmail.com>
2023-06-01 01:09:39 +0300

    Week4Tasks

--- OOPTask/src/com/oop/Library.java
+++
@@ -1,7 +0,0 @@
-package com.oop;
-import java.util.ArrayList;
-public class Library {
-    String libraryName;
-    int libraryCode;
-    ArrayList<String> books = new ArrayList<String>();
-}
--- OOPTask/src/com/oop/User.java
+++
@@ -1,9 +0,0 @@
-package com.oop;
-
-import java.util.ArrayList;
-
-public class User {
-    String userName;
-    String userID;
-    ArrayList<String> broowedBooks = new ArrayList<String>();
-}
